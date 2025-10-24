edcc50f88399f56a6fcc3e003bb6754a3d567f7b
$hiv@k <shiva_b2j@yahoo.com>
2025-10-24 06:45:38 +0530

    Vercel
    Vercel deployment issues fixed

--- vite.config.js
+++ vite.config.js
@@ -19,14 +19,7 @@ export default defineConfig({
         }
       }
     },
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    chunkSizeWarningLimit: 1000
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'framer-motion']
